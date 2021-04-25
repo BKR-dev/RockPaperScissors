6af74644ca18d18b3102e2c7ac268488f6a2e52f
booker <sudo@bkrdev.org>
2021-04-25 12:54:46 +0200

    yes

--- game.js
+++ game.js
@@ -7,8 +7,6 @@ const getUserChoice = userInput => {
     }
   }
   
-  console.log(getUserChoice("ROCK"));
-  
   const getComputerChoice =()=> {
     let choice=Math.floor(Math.random()*3);
     console.log(choice);
